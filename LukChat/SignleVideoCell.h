afa3cbeceeab197095e215a629b05f85b9672c70
Naveen <naveendungarwal@Naveens-MacBook-Pro.local>
2015-04-16 00:52:33 +0530

    single video screen added

---
+++ LukChat/SignleVideoCell.h
@@ -0,0 +1,14 @@
+//
+//  SignleVideoCell.h
+//  LukChat
+//
+//  Created by Naveen on 16/04/15.
+//  Copyright (c) 2015 Markus Haass Mac Mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignleVideoCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *thumbnail;
+@end
---
+++ LukChat/SignleVideoCell.m
@@ -0,0 +1,30 @@
+//
+//  SignleVideoCell.m
+//  LukChat
+//
+//  Created by Naveen on 16/04/15.
+//  Copyright (c) 2015 Markus Haass Mac Mini. All rights reserved.
+//
+
+#import "SignleVideoCell.h"
+
+@implementation SignleVideoCell
+
+-(void)setSelected:(BOOL)selected
+{
+    [super setSelected:selected];
+    if(selected)
+    {
+        self.thumbnail.layer.borderWidth = 2.0f;
+        self.thumbnail.layer.borderColor = [UIColor redColor].CGColor;
+        self.thumbnail.layer.masksToBounds = YES;
+    }
+    else
+    {
+        self.thumbnail.layer.borderWidth = 2.0f;
+        self.thumbnail.layer.borderColor = [UIColor yellowColor].CGColor;
+        self.thumbnail.layer.masksToBounds = YES;
+    }
+}
+
+@end
